e83ade83b657d105b7502c95f5ea40a83cbf671c
Rebecca Mark <rebecca.lmark@gmail.com>
2015-02-25 22:04:27 -0800

    added comments for clarity

--- src/com/mark/rebecca/KClosest.java
+++ src/com/mark/rebecca/KClosest.java
@@ -7,16 +7,18 @@ import java.util.*;
  */
 public class KClosest {
     public static List<Point> kClosestToOrigin(List<Point> points, int k) {
+        // create a priority queue, compared by distance to Origin.
+            // first argument is the size of initial queue, second is the comparator.
+            // we override comparator to enqueue in desired order.
         Queue<Point> pointsQueue = new PriorityQueue<Point>(points.size(),
                 new Comparator<Point>(){
-
                     @Override
                     public int compare(Point p1, Point p2) {
-                        // things that are farther away from origin are smaller.
-                        // compare words, return - num if smaller, 0 =, positive if pt 1 is greater than pt 2
+                        // things that are farther away from origin need to be evaluated as smaller int.
+                        // compare words, return - num if smaller, 0 if =, positive if pt 1 is greater than pt 2
                             Integer distanceOne = p1.distanceSquaredToOrigin();
                             Integer distanceTwo = p2.distanceSquaredToOrigin();
-                        // this returns -1 0 or 1 like what we need.
+                        // this returns -1 0 or 1 like the compare method.
                         return distanceOne.compareTo(distanceTwo);
                     }
                 });
@@ -24,9 +26,10 @@ public class KClosest {
         for(Point p: points) {
             pointsQueue.offer(p);
         }
+
+        // initializes an ArrayList of length k, where k = the desired num of closest points
         List<Point> returnList = new ArrayList<Point>(k);
-
-
+        // add a point from the beginning of the Queue to our final list K times.
         for(int i = 0; i < k; i ++ ){
             returnList.add(pointsQueue.poll());
         }
@@ -34,10 +37,12 @@ public class KClosest {
     }
 
     public static void main(String[] args) {
+        /// Initialize a LinkedList of a random number of points from 10 - 490;
         List<Point> myPoints = new LinkedList<Point>();
         Random random = new Random();
         int randomNumOfPoints = random.nextInt(490) + 10;
 
+        // randomly generates the x and y values for each point.
         for(int i = 0; i < randomNumOfPoints; i ++ ){
             int randomX = random.nextInt(100);
             int randomY = random.nextInt(100);
